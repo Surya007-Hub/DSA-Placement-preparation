cab111d145f1717b9c7ad71d36e5198eb41eef78
Surya <suryatharani2006@gmail.com>
2026-09-18 19:09:14 +0530

    Add find even numbers problem

---
+++ Arrays/find even numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findNumbers(self, nums: list[int]) -> int:
+        count=0
+        for num in nums:
+            if len(str(num))%2==0:
+                count+=1
+        return count
+
+
+if __name__ == "__main__":
+    nums = [12, 345, 2, 6, 7896]
+    print(Solution().findNumbers(nums))
